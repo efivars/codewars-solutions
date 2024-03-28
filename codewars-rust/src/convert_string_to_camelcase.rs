4f8ec2870e802b0f34706c53c799fcaa3ed364da
Kay <efivars@icloud.com>
2024-03-28 20:28:07 +0300

    Add convert_string_to_camelcase solution

---
+++ codewars-rust/src/convert_string_to_camelcase.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code)]
+/**
+ * (6 kyu)
+ * https://www.codewars.com/kata/517abf86da9663f1d2000003/train/rust
+ *
+ * Complete the method/function so that it converts dash/underscore delimited words
+ * into camel casing. The first word within the output should be capitalized only if
+ * the original word was capitalized (known as Upper Camel Case, also often referred
+ * to as Pascal case). The next words should be always capitalized.
+ */
+
+fn capitalize(s: &str) -> String {
+    let mut c = s.chars();
+
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().chain(c).collect(),
+    }
+}
+
+fn to_camel_case(text: &str) -> String {
+    let separators = ['_', '-'];
+
+    text.split(&separators)
+        .enumerate()
+        .map(|(index, str)| match index {
+            0 => str.to_owned(),
+            _ => capitalize(str),
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn converts_to_camelcase() {
+        let samples = [
+            ("the-stealth-warrior", "theStealthWarrior"),
+            ("The_Stealth_Warrior", "TheStealthWarrior"),
+            ("The_Stealth-Warrior", "TheStealthWarrior"),
+        ];
+
+        for (input, expected) in samples {
+            assert_eq!(to_camel_case(input), expected)
+        }
+    }
+}
--- codewars-rust/src/main.rs
+++ codewars-rust/src/main.rs
@@ -1,6 +1,7 @@
 mod spin_words;
 mod square_every_digit;
 mod duplicate_encoder;
+mod convert_string_to_camelcase;
 
 fn main() {
     println!("Hello, world!");
